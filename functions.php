5f947fe48311181a4297647bd5488e9638e59926
wkwkrnht <wkwkrnht@gmail.com>
2016-01-30 23:39:40 +0900

    モバイルメニュー：文字白抜き、絵文字削除、ごみポイ

--- functions.php
+++ functions.php
@@ -9,7 +9,9 @@ add_action( 'wp_enqueue_scripts', 'masonry_scripts');
 //function _scripts() {wp_enqueue_script( '', '', array(), false, false );}
 //add_action( 'wp_enqueue_scripts', '_scripts');
 
-// remove wp version param from any enqueued scripts
+// hide /?ver= & emoji
 function vc_remove_wp_ver_css_js( $src ) {if ( strpos( $src, 'ver=' ) )  $src = remove_query_arg( 'ver', $src );  return $src;}
 add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
