0a34a8aea650f8ab0dbad25315fd0de82fd2f88f
wkwkrnht <wkwkrnht@gmail.com>
2016-01-30 18:45:35 +0900

    タイル用プラグインcdnアドレスをコメントに

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
 <?php //style.css読み込み
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); }
+//<script src="https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js"></script>
