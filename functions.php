a8fbe7ade23c33d01892ff27e288fdfa48f968cc
wkwkrnht <wkwkrnht@gmail.com>
2016-02-04 11:26:50 +0900

    add:sitemap

--- functions.php
+++ functions.php
@@ -5,10 +5,9 @@ function theme_enqueue_styles() {wp_enqueue_style( 'parent-style', get_template_
 //masonry
 function masonry_scripts() {wp_enqueue_script( 'masonry', 'https://npmcdn.com/masonry-layout@4.0/dist/masonry.pkgd.min.js', array(), false, false );}
 add_action( 'wp_enqueue_scripts', 'masonry_scripts');
-//
+//テンプレ
 //function _scripts() {wp_enqueue_script( '', '', array(), false, false );}
 //add_action( 'wp_enqueue_scripts', '_scripts');
-
 // hide /?ver= & emoji
 function vc_remove_wp_ver_css_js( $src ) {if ( strpos( $src, 'ver=' ) )  $src = remove_query_arg( 'ver', $src );  return $src;}
 add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
@@ -16,3 +15,52 @@ add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 remove_action('wp_head', 'wp_generator');
 remove_action( ‘wp_head’, ‘print_emoji_detection_script’, 7 );
 remove_action( ‘wp_print_styles’, ‘print_emoji_styles’ );
+//sitemap
+function simple_sitemap(){
+  global $wpdb;
+  $args = array('depth'        => 0,
+    'show_date'    => NULL,
+    'date_format'  => get_option('date_format'),
+    'child_of'     => 0,
+    'exclude'      => NULL,
+    'include'      => NULL,
+    'title_li'           => '<span class="pagemap">固定ページの一覧</span>',
+    'echo'         => 1,
+    'authors'      => NULL,
+    'sort_column'  => 'menu_order, post_title',
+    'link_before'  => NULL,
+    'link_after'   => NULL,
+    'exclude_tree' => NULL );
+  echo '<div id="sitemap"><ul>';
+    wp_list_pages($args);
+  echo '</ul>';
+  $args = array('show_option_all'    => NULL,
+    'orderby'            => 'name',
+    'order'              => 'ASC',
+    'show_last_update'   => 0,
+    'style'              => 'list',
+    'show_count'         => 0,
+    'hide_empty'         => 1,
+    'use_desc_for_title' => 1,
+    'child_of'           => 0,
+    'feed'               => NULL,
+    'feed_type'          => NULL,
+    'feed_image'         => NULL,
+    'exclude'            => NULL,
+    'exclude_tree'       => NULL,
+    'include'            => NULL,
+    'hierarchical'       => true,
+    'title_li'           => '<span class="catmap">記事カテゴリ</span>',
+    'number'             => NULL,
+    'echo'               => 1,
+    'depth'              => 0,
+    'current_category'   => 0,
+    'pad_counts'         => 0,
+    'taxonomy'           => 'category',
+    'walker'             => 'Walker_Category' );
+        echo '<ul>';
+     echo wp_list_categories( $args );
+     echo '</ul>';
+  echo '</div>';
+}
+add_shortcode('sitemap', 'simple_sitemap');
