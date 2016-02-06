21d4dce0d924c3cb1bf36f3ad03a06e9cb082eb4
wkwkrnht <wkwkrnht@gmail.com>
2016-02-06 21:38:21 +0900

    クラス替えの準備

---
+++ home.php
@@ -0,0 +1,24 @@
+<?phpget_header(); ?>
+ <div id="primary" class="content-area">
+   <main id="main" class="site-main" role="main">
+   <?php if ( have_posts() ) : ?>
+     <?php if ( is_home() && ! is_front_page() ) : ?>
+       <header>
+         <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+       </header>
+     <?php endif; ?>
+     <?php
+     while ( have_posts() ) : the_post();
+       get_template_part( 'content', get_post_format() );
+     endwhile;
+     the_posts_pagination( array(
+       'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+       'next_text'          => __( 'Next page', 'twentyfifteen' ),
+       'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
+     ) );
+   else :
+     get_template_part( 'content', 'none' );
+   endif;?>
+   </main><!-- .site-main -->
+ </div><!-- .content-area -->
+<?php get_footer(); ?>
---
+++ search.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+	<section id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+		<?php if ( have_posts() ) : ?>
+			<header class="page-header">
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyfifteen' ), get_search_query() ); ?>
+                <?php if ($wp_query->found_posts > 0){echo echo '('.$wp_query->found_posts.' 件)';}?>
+				</h1>
+			</header><!-- .page-header -->
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php get_template_part( 'content', 'search' );
+			endwhile;
+			the_posts_pagination( array(
+				'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
+				'next_text'          => __( 'Next page', 'twentyfifteen' ),
+				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',));
+		else :
+			get_template_part( 'content', 'none' );
+		endif;?>
+		</main><!-- .site-main -->
+	</section><!-- .content-area -->
+<?php get_footer(); ?>
