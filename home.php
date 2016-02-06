56a3de2d7b669438031c9a2a95265d26c04f7775
wkwkrnht <wkwkrnht@gmail.com>
2016-02-06 21:47:10 +0900

    bugfix

--- home.php
+++ home.php
@@ -1,4 +1,4 @@
-<?phpget_header(); ?>
+<?php get_header(); ?>
  <div id="primary" class="content-area">
    <main id="main" class="site-main" role="main">
    <?php if ( have_posts() ) : ?>
@@ -7,15 +7,13 @@
          <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
        </header>
      <?php endif; ?>
-     <?php
-     while ( have_posts() ) : the_post();
+     <?php while ( have_posts() ) : the_post();
        get_template_part( 'content', get_post_format() );
      endwhile;
      the_posts_pagination( array(
        'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
        'next_text'          => __( 'Next page', 'twentyfifteen' ),
-       'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',
-     ) );
+       'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',) );
    else :
      get_template_part( 'content', 'none' );
    endif;?>
