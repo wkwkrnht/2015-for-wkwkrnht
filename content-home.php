12039e4d5cf2b41f669e61828feee688575cf61d
wkwkrnht <wkwkrnht@gmail.com>
2016-02-06 23:05:26 +0900

    fix

--- content-home.php
+++ content-home.php
@@ -1,20 +1,14 @@
-<?php?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfifteen_post_thumbnail();?>
 	<header class="entry-header">
-		<?php
-			if ( is_single() ) :
+		<?php if ( is_single() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			else :
 				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
-			endif;
-		?>
+			endif;?>
 	</header><!-- .entry-header -->
 	<div class="entry-content">
-		<?php//%s=Name of current post
-			the_content( sprintf(
-				__( 'Continue reading %s', 'twentyfifteen' ),
-				the_title( '<span class="screen-reader-text">', '</span>', false )) );
+		<?php the_content( sprintf( __( 'Continue reading %s', 'twentyfifteen' ),the_title( '<span class="screen-reader-text">', '</span>', false )) );//%s=Name of current post
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
 				'after'       => '</div>',
