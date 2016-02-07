f86b8452d71aecd2366f8c0d181eb28841e4b753
wkwkrnht <wkwkrnht@gmail.com>
2016-02-07 15:38:27 +0900

    test

--- content-home.php
+++ content-home.php
@@ -4,17 +4,17 @@
 		<?php if ( is_single() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			else :
-				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+				the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url(get_permalink())),'</a></h2>');
 			endif;?>
 	</header><!-- .entry-header -->
 	<div class="home-content">
-		<?php the_content( sprintf( __( 'Continue reading %s', 'twentyfifteen' ),the_title( '<span class="screen-reader-text">', '</span>', false )) );//%s=Name of current post
-			wp_link_pages( array(
-				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
+		<?php the_content(sprintf( __('Continue reading %s','twentyfifteen'),the_title('<span class="screen-reader-text">','</span>',false)));//%s=Name of current post
+			wp_link_pages(array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' . __('Pages:','twentyfifteen').'</span>',
 				'after'       => '</div>',
 				'link_before' => '<span>',
 				'link_after'  => '</span>',
-				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
+				'pagelink'    => '<span class="screen-reader-text">' . __('Page', 'twentyfifteen').' </span>',
 				'separator'   => '<span class="screen-reader-text">, </span>',) );?>
 	</div><!-- .entry-content -->
 	<footer class="home-footer">
