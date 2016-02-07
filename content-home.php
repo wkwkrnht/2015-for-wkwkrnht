dd63ad41963b7dfcd260658d56a05a98bd11b270
wkwkrnht <wkwkrnht@gmail.com>
2016-02-07 15:17:05 +0900

    job change

--- content-home.php
+++ content-home.php
@@ -1,14 +1,13 @@
-<div class="homecard">
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfifteen_post_thumbnail();?>
-	<header class="entry-header">
+	<header class="home-header">
 		<?php if ( is_single() ) :
 				the_title( '<h1 class="entry-title">', '</h1>' );
 			else :
 				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
 			endif;?>
 	</header><!-- .entry-header -->
-	<div class="entry-content">
+	<div class="home-content">
 		<?php the_content( sprintf( __( 'Continue reading %s', 'twentyfifteen' ),the_title( '<span class="screen-reader-text">', '</span>', false )) );//%s=Name of current post
 			wp_link_pages( array(
 				'before'      => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentyfifteen' ) . '</span>',
@@ -18,12 +17,8 @@
 				'pagelink'    => '<span class="screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>%',
 				'separator'   => '<span class="screen-reader-text">, </span>',) );?>
 	</div><!-- .entry-content -->
-	<?php if ( is_single() && get_the_author_meta( 'description' ) ) :
-			get_template_part( 'author-bio' );
-		endif;?>
-	<footer class="entry-footer">
+	<footer class="home-footer">
 		<?php twentyfifteen_entry_meta(); ?>
 		<?php edit_post_link( __( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
 	</footer><!-- .entry-footer -->
 </article><!-- #post-## -->
-</div>
