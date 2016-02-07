6909df57af082bab30ea7974e2f7cd0c7b4254f7
wkwkrnht <wkwkrnht@gmail.com>
2016-02-07 15:51:46 +0900

    some changes

--- home.php
+++ home.php
@@ -2,24 +2,24 @@
  <div id="primary" class="content-area">
    <main id="main" class="site-main" role="main">
    <?php if ( have_posts() ) : ?>
-     <?php if ( is_home() && ! is_front_page() ) : ?>
+     <?php if (is_home() && ! is_front_page()) : ?>
        <header>
          <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
        </header>
      <?php endif; ?>
-     <?php while ( have_posts() ) : the_post();
-       if ( is_home() || is_front_page() ) :
-         get_template_part( 'content-home', get_post_format() );
+     <?php while (have_posts()) : the_post();
+       if (is_home() || is_front_page() ) :
+         get_template_part('content-home', get_post_format());
         else:
-         get_template_part( 'content', get_post_format() );
+         get_template_part('content', get_post_format());
        endif;
      endwhile;
-     the_posts_pagination( array(
-       'prev_text'          => __( 'Previous page', 'twentyfifteen' ),
-       'next_text'          => __( 'Next page', 'twentyfifteen' ),
-       'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyfifteen' ) . ' </span>',) );
+     the_posts_pagination(array(
+       'prev_text'          => __('Previous page','twentyfifteen'),
+       'next_text'          => __('Next page','twentyfifteen'),
+       'before_page_number' => '<span class="meta-nav screen-reader-text">'.__('Page','twentyfifteen').'</span>',));
    else :
-     get_template_part( 'content', 'none' );
+     get_template_part('content','none');
    endif;?>
    </main><!-- .site-main -->
  </div><!-- .content-area -->
