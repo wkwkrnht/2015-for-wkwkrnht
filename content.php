7bcda119be98f4ca91b83e2ec27e1d6ca1eca3cf
wkwkrnht <wkwkrnht@gmail.com>
2016-02-08 23:39:09 +0900

    change

--- content.php
+++ content.php
@@ -6,8 +6,22 @@
 			else :
 				the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">',esc_url(get_permalink())), '</a></h2>');
 			endif;?>
-		<?php the_bread(); ?>
 	</header><!-- .entry-header -->
+	<?php if ( !is_home() && !is_front_page() ) :
+		$cat = is_single() ? get_the_category() : array(get_category($cat));
+	if($cat && !is_wp_error($cat)){
+	    $par = get_category($cat[0]->parent);
+	    echo '<div id="breadcrumb">';
+	    echo '<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_bloginfo('url').'" itemprop="url"><span itemprop="title">ホーム</span></a><span class="sp">&gt;</span></div>';
+	    while($par && !is_wp_error($par) && $par->term_id != 0){
+	         $echo = '<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($par->term_id).'" itemprop="url"><span itemprop="title">'.$par->name.'</span></a><span class="sp">&gt;</span></div>'.$echo;
+	         $par = get_category($par->parent);
+	    }
+	    echo $echo.'<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">'.$cat[0]->name.'</span></a></div>';
+
+	    echo '</div><!-- /#breadcrumb -->';
+	}
+	endif; ?>
 	<div class="entry-content">
 		<?php the_content( sprintf( __( 'Continue reading %s', 'twentyfifteen' ),the_title( '<span class="screen-reader-text">', '</span>', false )) );//%s=Name of current post
 			wp_link_pages( array(
