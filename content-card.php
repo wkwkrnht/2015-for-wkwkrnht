81c5627d1da9f3392427fb5b62a44b9cda707112
wkwkrnht <wkwkrnht@gmail.com>
2016-02-21 16:40:23 +0900

    fix

--- content-card.php
+++ content-card.php
@@ -1,9 +1,13 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfifteen_post_thumbnail();?>
 	<div class="card-content">
-		<?php　the_title(sprintf('<div class="title"><a href="%s" rel="bookmark">',esc_url(get_permalink())),'</a></div>');?>
+		<?php if(is_single() ) :
+				the_title('<h1 class="entry-title">', '</h1>');
+			else :
+				the_title(sprintf('<h2 class="entry-title"><a href="%s" rel="bookmark">',esc_url(get_permalink())), '</a></h2>');
+			endif;?>
 		<span class=metadate><?php echo get_the_date();echo ('（');echo yumepyon_diff();echo('）')?></span>
-		<?php printf('<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',_x('Author','Used before post author name.','twentyfifteen'),esc_url(get_author_posts_url(get_the_author_meta( 'ID' ))),get_the_author());?>
-		<?php edit_post_link( __('Edit','twentyfifteen'),'<span class="edit-link"></span>');?>
+		<?php printf('<span class="byline"><span class="author vcard"><span class="screen-reader-text">%1$s </span><a class="url fn n" href="%2$s">%3$s</a></span></span>',_x('Author','Used before post author name.','twentyfifteen'),esc_url(get_author_posts_url(get_the_author_meta( 'ID' ))),get_the_author());
+		 edit_post_link( __('Edit','twentyfifteen'),'<span class="edit-link"></span>');?>
 	</div><!-- .card-content -->
 </article><!-- #post-## -->
