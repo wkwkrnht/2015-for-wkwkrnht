cba1a95e0eda892d244dadddfdc67bef7ce08b27
wkwkrnht <wkwkrnht@gmail.com>
2016-02-26 00:05:29 +0900

    fix

--- sns/ogp.php
+++ sns/ogp.php
@@ -5,11 +5,10 @@ if(have_posts()): while(have_posts()): the_post();
 endwhile; endif;
   echo '<meta property="og:title" content="'; the_title(); echo '">';echo "\n";
   echo '<meta property="og:url" content="'; the_permalink(); echo '">';echo "\n";
-} else {
+}else{
   echo '<meta property="og:description" content="'; bloginfo('description'); echo '">';echo "\n";
   echo '<meta property="og:title" content="'; bloginfo('name'); echo '">';echo "\n";
-  echo '<meta property="og:url" content="'; bloginfo('url'); echo '">';echo "\n";
-}
+  echo '<meta property="og:url" content="'; bloginfo('url'); echo '">';echo "\n";}
 $str = $post->post_content;
 $searchPattern = '/<img.*?src=(["\'])(.+?)\1.*?>/i';
 if (is_single()){
@@ -17,15 +16,14 @@ if (is_single()){
     $image_id = get_post_thumbnail_id();
     $image = wp_get_attachment_image_src( $image_id, 'full');
     echo '<meta property="og:image" content="'.$image[0].'">';echo "\n";
-  } else if ( preg_match( $searchPattern, $str, $imgurl ) && !is_archive()) {
+  }else if(preg_match($searchPattern, $str, $imgurl ) && !is_archive()) {
     echo '<meta property="og:image" content="'.$imgurl[2].'">';echo "\n";
-  } else {
-    $ogp_image = get_stylesheet_directory_uri().'/img/no-img.png';
+  }else{
+    $ogp_image = '/img/no-img.png';
     echo '<meta property="og:image" content="'.$ogp_image.'">';echo "\n";}
-} else {
+}else{
   if (get_header_image()){echo '<meta property="og:image" content="'.get_header_image().'">';echo "\n";
-  } else {echo '<meta property="og:image" content="'.get_stylesheet_directory_uri().'/screenshot.png">';echo "\n";}
-}?>
+  } else {echo '<meta property="og:image" content="/screenshot.png">';echo "\n";}}?>
 <meta property="og:site_name" content="<?php bloginfo('name');?>">
 <meta property="og:locale" content="ja_JP" />
 <meta property="fb:admins" content="100010188733942">
@@ -37,7 +35,7 @@ if(have_posts()): while(have_posts()): the_post();
 endwhile; endif;
   echo '<meta name="twitter:title" content="'; the_title(); echo '">';echo "\n";
   echo '<meta name="twitter:url" content="'; the_permalink(); echo '">';echo "\n";
-  } else {
+  }else{
   echo '<meta name="twitter:description" content="'; bloginfo('description'); echo '">';echo "\n";
   echo '<meta name="twitter:title" content="'; bloginfo('name'); echo '">';echo "\n";
   echo '<meta name="twitter:url" content="'; bloginfo('url'); echo '">';echo "\n";}
@@ -53,17 +51,16 @@ if (is_single()){
     $img_url = $imgurl[2];
     echo '<meta name="twitter:image" content="'.$imgurl[2].'">';echo "\n";
   }else{
-    $ogp_image = get_stylesheet_directory_uri().'/img/no-img.png';
+    $ogp_image = '/img/no-img.png';
     $img_url = $ogp_image;
     echo '<meta name="twitter:image" content="'.$ogp_image.'">';echo "\n";}
-}else{
+}else{//get_stylesheet_directory_uri().
   if (get_header_image()){
     $img_url = get_header_image();
     echo '<meta name="twitter:image" content="'.$img_url.'">';echo "\n";
   } else {
-    $img_url = get_stylesheet_directory_uri().'/screenshot.png';
-    echo '<meta name="twitter:image" content="'.$img_url.'">';echo "\n";}
-}
+    $img_url = '/screenshot.png';
+    echo '<meta name="twitter:image" content="'.$img_url.'">';echo "\n";}}
 preg_match( '/https?:\/\/(.+?)\//i', admin_url(), $results );
 list($width,$height) = getimagesize($img_url);?>
 <meta name="twitter:domain" content="<?php echo $results[1] ?>">
