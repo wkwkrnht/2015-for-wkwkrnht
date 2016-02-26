0cb6cea815c8da142c9653685f2aa0d16fadf494
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 00:43:42 +0900

    fix

--- parts/related.php
+++ parts/related.php
@@ -1,17 +1,17 @@
+<?php $categories = get_the_category($post->ID);
+$category_ID = array();
+foreach($categories as $category):
+  array_push($category_ID, $category -> cat_ID);
+endforeach;
+$args = array(
+  'post__not_in' => array($post -> ID),
+  'posts_per_page'=> 5,
+  'category__in' => $category_ID,
+  'orderby' => 'rand',);
+$query = new WP_Query($args);
+if($query -> have_posts()): while ($query -> have_posts()) : $query -> the_post();wp_reset_postdata();?>
 <div class="related-entry">
   <h3>関連記事</h3>
-  <?php $categories = get_the_category($post->ID);
-  $category_ID = array();
-  foreach($categories as $category):
-    array_push($category_ID, $category -> cat_ID);
-  endforeach;
-  $args = array(
-    'post__not_in' => array($post -> ID),
-    'posts_per_page'=> 5,
-    'category__in' => $category_ID,
-    'orderby' => 'rand',);
-  $query = new WP_Query($args);
-  if($query -> have_posts()): while ($query -> have_posts()) : $query -> the_post();?>
   <div class="content">
     <div class="thumb">
       <a href="<?php the_permalink() ?>" title="<?php the_title_attribute();?>"><?php if(has_post_thumbnail()){echo get_the_post_thumbnail($post->ID);}else{echo('<img src="img/no-image.png" alt="NO IMAGE" title="NO IMAGE" width="100px" />');}?></a>
