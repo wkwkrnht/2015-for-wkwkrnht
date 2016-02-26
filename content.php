cce367fd12446444a6f7d32202c80fce4aa20fed
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 00:07:54 +0900

    fix

--- content.php
+++ content.php
@@ -5,9 +5,9 @@
 				the_title('<h1 class="entry-title">','</h1>');
 			else :
 				the_title(sprintf('<h1 class="entry-title"><a href="%s" rel="bookmark">',esc_url(get_permalink())),'</a></h1>');
-			endif;?>
+			endif;
+		get_template_part('parts/bread');?>
 	</header><!-- .entry-header -->
-	<?php get_template_part('parts/bread');?>
 	<div class="entry-content">
 		<?php the_content(sprintf(__('Continue reading %s','twentyfifteen'),the_title('<span class="screen-reader-text">','</span>',false)));//%s=Name of current post
 			wp_link_pages(array(
--- parts/related.php
+++ parts/related.php
@@ -4,7 +4,7 @@
   $category_ID = array();
   foreach($categories as $category):
     array_push($category_ID, $category -> cat_ID);
-  endforeach ;
+  endforeach;
   $args = array(
     'post__not_in' => array($post -> ID),
     'posts_per_page'=> 5,
@@ -15,11 +15,7 @@
   <div class="content">
     <div class="thumb">
       <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-        <?php if(has_post_thumbnail()):
-          echo get_the_post_thumbnail($post->ID);
-        else:
-          echo ('<img src="img/no-image.png" alt="NO IMAGE" title="NO IMAGE" width="100px" />');
-        endif;?>
+        <?php if(has_post_thumbnail()){echo get_the_post_thumbnail($post->ID);}else{echo('<img src="img/no-image.png" alt="NO IMAGE" title="NO IMAGE" width="100px" />');}?>
       </a>
       <div class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></div>
     </div>
