be748ec1a4e009e8d9941ac10d2d8792cd18d0d0
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 16:15:41 +0900

    fix

--- javascript.js
+++ javascript.js
@@ -1,4 +1,4 @@
 jQuery(function(){
   //ページトップへ移動するボタン
-  jQuery('.move-page-top').click(function(){jQuery('body,html').animate({scrollTop: 0},800);});
+  jQuery('#move-page-top').click(function(){jQuery('body,html').animate({scrollTop:0},slow);});
 });
