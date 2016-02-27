99e6375af1484230698b779587b43cf471db66d8
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 09:04:16 +0900

    fix

--- parts/related.php
+++ parts/related.php
@@ -11,8 +11,8 @@ $query = new WP_Query($args);
    if($query -> have_posts()):
    while ($query -> have_posts()) : $query -> the_post();?>
     <div class="related-entry">
-      <div class="related-entry-thumb">
-  <a href="<?php the_permalink() ?>" title="<?php the_title_attribute();?>">
+      <div class="thumb">
+  <a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>">
         <?php if (has_post_thumbnail()):
          echo get_the_post_thumbnail($post->ID,'thumb100');
        else:?>
@@ -20,14 +20,14 @@ $query = new WP_Query($args);
         <?php endif;?>
         </a>
       </div><!-- /.related-entry-thumb -->
-      <div class="related-entry-content">
-        <h4 class="related-entry-title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
-        <p class="related-entry-read"><a href="<?php the_permalink();?>">記事を読む</a></p>
+      <div class="content">
+        <h4 class="title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h4>
+        <p class="entry-read"><a href="<?php the_permalink();?>">Read</a></p>
       </div><!-- /.related-entry-content -->
     </div><!-- /.new-entry -->
   <?php endwhile;
    else:
   echo '<p>記事はありませんでした</p>'
-endif;?>
-<?php wp_reset_postdata();?>
+endif;
+wp_reset_postdata();?>
 <br style="clear:both;">
