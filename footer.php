06f8c562aeaf7453db6e29aa20098e9e78d82f74
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 11:38:22 +0900

    move top button

---
+++ footer.php
@@ -0,0 +1,12 @@
+		<div class="page-top"><a class="move-page-top">Λ</a></div>
+	</div><!-- .site-content -->
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<?php do_action('twentyfifteen_credits');?>
+			<a href="<?php echo esc_url( __('https://wordpress.org/','twentyfifteen'));?>"><?php printf( __('Proudly powered by %s','twentyfifteen'),'WordPress');?></a>
+		</div><!-- .site-info -->
+	</footer><!-- .site-footer -->
+</div><!-- .site -->
+<?php wp_footer(); ?>
+</body>
+</html>
