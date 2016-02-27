e4bd13ceaf98ae337c554b468f584bbb12e35611
wkwkrnht <wkwkrnht@gmail.com>
2016-02-27 16:32:54 +0900

    fix

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-		<div class="page-top"><a id="move-page-top"></a></div>
+		<div class="page-top"><a id="move-page-top">∧</a></div>
 	</div><!-- .site-content -->
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
