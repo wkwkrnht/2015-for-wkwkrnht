dad617153d6ed959a2b7c2bf1488002989d4eef1
wkwkrnht <wkwkrnht@gmail.com>
2016-02-28 15:34:46 +0900

    fix

--- javascript.js
+++ javascript.js
@@ -10,26 +10,12 @@ jQuery(function(){
         this.id = "toc_" + idcount;
         idcount++;
         var level = 0;
-        if(this.nodeName.toLowerCase() == "h2") {
-            level = 1;
-        } else if(this.nodeName.toLowerCase() == "h3") {
-            level = 2;
-        } else if(this.nodeName.toLowerCase() == "h4") {
-            level = 3;
-        } else if(this.nodeName.toLowerCase() == "h5") {
-            level = 4;
-        }
-        while(currentlevel < level) {
-            toc += "<ul>";
-            currentlevel++;
-        }
-        while(currentlevel > level) {
-            toc += "</ul>";
-            currentlevel--;
-        }
+        if(this.nodeName.toLowerCase() == "h2"){level = 1;}else if(this.nodeName.toLowerCase() == "h3"){level = 2;}else if(this.nodeName.toLowerCase() == "h4"){level = 3;}else if(this.nodeName.toLowerCase() == "h5"){level = 4;}
+        while(currentlevel < level) {toc += "<ul>";currentlevel++;}
+        while(currentlevel > level) {toc += "</ul>";currentlevel--;}
         toc += '<li><a href="#' + this.id + '">' + jQuery(this).html() + "<\/a><\/li>\n";
     });
-    if(toc){ toc = '<div class="mokuji_wrap"><div class="mokuji">【目次】</div>' + toc + '</div>'; }
+    if(toc){toc = '<div class="mokuji_wrap"><div class="mokuji">目次</div>' + toc + '</div>';}
     jQuery("#toc").html(toc);
 
     //ページ内リンク#非表示。加速スクロール
@@ -38,7 +24,7 @@ jQuery(function(){
                 href= jQuery(this).attr("href"),
                 target = jQuery(href == "#" || href == "" ? 'html' : href),
                 position = target.offset().top;
-        jQuery("html, body").animate({scrollTop:position}, speed, "swing");
+        jQuery("html, body").animate({scrollTop:position},speed,"swing");
         return false;
     });
   });
