0e40de44a26737e411a8180c2436e43324e7d804
wkwkrnht <wkwkrnht@gmail.com>
2016-03-04 22:12:23 +0900

    fix

--- parts/related.php
+++ parts/related.php
@@ -2,7 +2,7 @@
 foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
 $args=array('posts_per_page'=>8,'post__not_in'=>array($post->ID),'category__in'=>$category_ID,'orderby'=>'rand',);
 $query=new WP_Query($args);
-if(has_post_thumbnail()):$image=get_the_post_thumbnail($post->ID);else:$image='http://wkwkrnht.wp.xdomain.jp/wp-content/themes/2015-for-wkwkrnht/img/no-img.png';endif;
+if(has_post_thumbnail()):$image=get_the_post_thumbnail($post->ID);else:$image='../img/no-img.png';endif;
   if($query -> have_posts()):
    while($query -> have_posts()):$query->the_post();?>
    <style>.thumb{background-image=url("<?php echo($image)?>");}</style>
