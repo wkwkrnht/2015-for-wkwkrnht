61094876d8569f117d01e9d944d81ddac177ff99
wkwkrnht <wkwkrnht@gmail.com>
2016-03-06 22:05:01 +0900

    fix

--- parts/related.php
+++ parts/related.php
@@ -2,10 +2,10 @@
 foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
 $args=array('posts_per_page'=>8,'post__not_in'=>array($post->ID),'category__in'=>$category_ID,'orderby'=>'rand',);
 $query=new WP_Query($args);
-  if($query -> have_posts()):?>
+  if($query->have_posts()):?>
     <div id="slide">
       <?php while($query -> have_posts()):$query->the_post();?>
-        <div class="swiper-slide content"><a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>">
+        <div class="slide"><a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>">
           <?php if(has_post_thumbnail()):echo get_the_post_thumbnail($post->ID,'thumbnail',array('class'=>'thumb'));else:echo'<img src="http://wkwkrnht.wp.xdomain.jp/wp-content/themes/2015-for-wkwkrnht/img/no-img.png" class="thumb" alt="no_thumbnail"/>';endif;?>
           <div class="title"><?php the_title();?></div>
         </a></div>
@@ -16,10 +16,10 @@ $query=new WP_Query($args);
     <?php $args = array('numberposts'=>8,'orderby'=>'rand','post_status'=>'publish','offset'=>1);
     $rand_posts = get_posts($args);
     foreach($rand_posts as $post):?>
-      <div class="swiper-slide"><div class="content"><a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>">
+      <div class="swiper-slide"><a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>">
         <?php if(has_post_thumbnail()):echo get_the_post_thumbnail($post->ID,'thumbnail',array('class'=>'thumb'));else:echo'<img src="http://wkwkrnht.wp.xdomain.jp/wp-content/themes/2015-for-wkwkrnht/img/no-img.png" class="thumb" alt="no_thumbnail"/>';endif;?>
         <div class="title"><?php the_title();?></div>
-      </a></div></div>
+      </a></div>
     <?php endforeach;?>
     </div>
   <?php endif;?>
