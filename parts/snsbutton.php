6b9bc3d8372495e4c17c6d474a13fda35376084a
wkwkrnht <wkwkrnht@gmail.com>
2016-03-06 18:04:56 +0900

    fix

--- parts/snsbutton.php
+++ parts/snsbutton.php
@@ -1,5 +1,5 @@
-<a id="pre" href="#"><div class="pre-1"></div></a>
-<div class="buttonarea">
+<a id="pre" href="#"><div class="pre"></div></a>
+<div id="slide" class="buttonarea">
   <a href="https://twitter.com/share?url=<?php echo get_permalink()?>&amp;text=<?php echo trim(wp_title('',false));?>&amp;via=wkwkrnht`" onclick="window.open(encodeURI(decodeURI(this.href),'tweetwindow','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;" target="_blank"><div class="tweet"></div></a>
   <a href="http://www.facebook.com/share.php?u=<?php echo trim(wp_title('',false));?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="fblike"></div></a>
   <a href="https://plus.google.com/share?url=<?php echo get_permalink()?>" target="_blank"><div class="g1"></div></a>
@@ -10,4 +10,4 @@
   <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink()?>&amp;title=<?php echo trim(wp_title('',false));?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="linkedin"></div></a>
   <a href="https://push.dog/subscribe?url=<?php echo bloginfo('url')?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><img src="<?php echo get_stylesheet_directory_uri();?>/img/pushdog.png" class="pushdog"/></a>
 </div>
-<a id="next" href="#"><div class="next-1"></div></a>
+<a id="next" href="#"><div class="next"></div></a>
