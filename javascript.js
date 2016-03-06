f1317c837fe51231cae2c773260f95b73d73e4d9
wkwkrnht <wkwkrnht@gmail.com>
2016-03-06 14:38:13 +0900

    fix

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)}(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create','UA-XXXXX-Y','auto');ga('send','pageview');
 jQuery(function(){
   //top-button
   jQuery('#page-top').click(function(){jQuery('body,html').animate({scrollTop:0},500);return false;});
@@ -5,8 +9,8 @@ jQuery(function(){
   $('#toc').toc({
     'selectors':'.entry-content h2,.entry-content h3',//目次として表示する要素のCSSセレクターを指定
     'container':'body',//コンテナ要素
-    'smoothScrolling': true, //スムーズなスクロールを有効にするか
-    'prefix': 'toc',//アンカータグとクラス名のプレフィックス
+    'smoothScrolling':true, //スムーズなスクロールを有効にするか
+    'prefix':'toc',//アンカータグとクラス名のプレフィックス
     'onHighlight':function(el){},//新しいセクションがハイライトされたときの動作を指定
     'highlightOnScroll':true,//ハイライトを有効にするか
     'highlightOffset':100,//次の見出しのトリガーとなるオフセット位置
