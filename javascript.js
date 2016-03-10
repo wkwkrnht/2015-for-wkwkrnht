6a13777035f0cf56e379450ce7532798de8b95a9
wkwkrnht <wkwkrnht@gmail.com>
2016-03-10 19:11:02 +0900

    change

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)}(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create',<?php echo get_option('analytics_code');?>,'auto');ga('send','pageview');
+ga('create',<?php echo get_option('ganalytics');?>,'auto');ga('send','pageview');
 jQuery(function(){
   //top-button
   jQuery('#page-top').click(function(){jQuery('body,html').animate({scrollTop:0},500);return false;});
@@ -23,4 +23,38 @@ jQuery(function(){
 	  jQuery('#next').click(function(event){event.preventDefault();jQuery('#slide').animate({scrollLeft:'+=455'},'slow');});
 	  jQuery('#pre').click(function(event){event.preventDefault();jQuery('#slide').animate({scrollRight:'+=455'},'slow');});});
   //next
+});
+jQuery(document).ready(function(jQuery){
+	function getRightClick(e){var rightclick;
+		if(!e) var e = window.event;
+		if(e.which) rightclick = (e.which == 3);
+		else if(e.button) rightclick = (e.button == 2);
+		return rightclick;
+	}
+	function getSelectionText(){var text = "";
+	    if(window.getSelection){text = window.getSelection().toString();
+	    }else if(document.selection && document.selection.type != "Control"){text = document.selection.createRange().text;}
+	    return text;
+	}
+	jQuery('.entry-content').mousedown(function (event){
+		jQuery('body').attr('mouse-top',event.clientY+window.pageYOffset);
+		jQuery('body').attr('mouse-left',event.clientX);
+		if(!getRightClick(event) && getSelectionText().length > 0){jQuery('.twtshare').remove();document.getSelection().removeAllRanges();}
+	});
+  	jQuery('.entry-content').mouseup(function (event){var t=jQuery(event.target);var st=getSelectionText();
+       	if(st.length > 3 && !getRightClick(event)){mts = jQuery('body').attr('mouse-top');
+      	 	mte = event.clientY+window.pageYOffset;
+      	 	if(parseInt(mts) < parseInt(mte)) mt = mts;
+      	 		else mt = mte;
+      	 	mlp = jQuery('body').attr('mouse-left');
+      	 	mrp = event.clientX;
+      	 	ml = parseInt(mlp)+(parseInt(mrp)-parseInt(mlp))/2;
+      	 	sl = window.location.href.split('?')[0];
+      	 	maxl = 107;
+      	 	st = st.substring(0,maxl);
+      	 	st = st+' @wpmudev';
+      	 	jQuery('body').append("<a href=\"https://twitter.com/share?url="+encodeURIComponent(sl)+"&text="+encodeURIComponent(st)+"\" class='twtshare icon-social-twitter' onclick=\"window.open(this.href, \'\', \'menubar=no,toolbar=no,resizable=yes,scrollbars=yes,height=300,width=600\');return false;\"></a>");
+      	 	jQuery('.twtshare').css({position: 'absolute',top: parseInt(mt)-60,left: parseInt(ml)});
+     	}
+  });
 });
--- select-and-tweet/select-and-tweet.php
+++ select-and-tweet/select-and-tweet.php
@@ -8,13 +8,5 @@ Author: Daniel Pataki
 Author URI: http://www.danielpataki.com/
 Text Domain: select-and-tweet
 */
-
-
-function sat_enqueue_assets() {
-    if ( is_single() ) {
-	   wp_enqueue_style( 'select-and-tweet', plugins_url( '/select-and-tweet.css', __FILE__ ) );
-        wp_enqueue_script( 'select-and-tweet', plugins_url( '/select-and-tweet.js', __FILE__ ), array( 'jquery' ), '1.0', true  );
-    }
-}
-
-add_action( 'wp_enqueue_scripts', 'sat_enqueue_assets' );
+function sat_enqueue_assets(){if(is_single()){wp_enqueue_style('select-and-tweet',plugins_url('/select-and-tweet.css', __FILE__ ));wp_enqueue_script('select-and-tweet',plugins_url('/select-and-tweet.js', __FILE__ ), array('jquery'),'1.0',true );}}
+add_action('wp_enqueue_scripts','sat_enqueue_assets');
