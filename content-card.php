3202be2345293c101a974e74345a5bb07e577519
wkwkrnht <wkwkrnht@gmail.com>
2016-03-11 00:10:35 +0900

    fix

--- content-card.php
+++ content-card.php
@@ -1,9 +1,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class();?>>
 	<div class="card-list">
-		<div class="thumb"><?php twentyfifteen_post_thumbnail();?></div>
-		<div class="content">
-			<div class="title"><?php if(is_single()):the_title();else:the_title(sprintf('<a href="%s" rel="bookmark">',esc_url(get_permalink())),'</a>');endif;?></div>
-			<?php echo'<div class="meta">';twentyfifteen_entry_meta();echo'</div>';?>
+		<div class="card-thumb"><?php twentyfifteen_post_thumbnail();?></div>
+		<div class="card-content">
+			<div class="card-title"><?php if(is_single()):the_title();else:the_title(sprintf('<a href="%s" rel="bookmark">',esc_url(get_permalink())),'</a>');endif;?></div>
+			<?php echo'<div class="card-meta">';twentyfifteen_entry_meta();echo'</div>';?>
 		</div>
 	</div>
 </article>
