557a2dc41d5dcd9237d153b8359727a15a53cebd
wkwkrnht <wkwkrnht@gmail.com>
2016-03-14 17:22:08 +0900

    fix

--- parts/bread.php
+++ parts/bread.php
@@ -1,5 +1,5 @@
-<div id="bread"><?php $cat = is_single()?get_the_category():array(get_category($cat));
-if($cat && !is_wp_error($cat)){$par=get_category($cat[0]->parent);
-  echo'<div id="bread"><div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_bloginfo('url').'" itemprop="url"><span itemprop="title">HOME</span></a><span class="sp">/</span></div>';
+<div id="bread"><?php $cat=is_single()?get_the_category():array(get_category($cat));
+if($cat&&!is_wp_error($cat)){$par=get_category($cat[0]->parent);
+  echo'<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_bloginfo('url').'" itemprop="url"><span itemprop="title">HOME</span></a><span class="sp">/</span></div>';
   while($par&&!is_wp_error($par)&&$par->term_id!=0){$echo='<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($par->term_id).'" itemprop="url"><span itemprop="title">'.$par->name.'</span></a><span class="sp">/</span></div>'.$echo;$par=get_category($par->parent);}
-  echo $echo.'<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">'.$cat[0]->name.'</span></a></div>';echo'</div>';}</div>
+  echo $echo.'<div itemtype="http://data-vocabulary.org/Breadcrumb" itemscope=""><a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">'.$cat[0]->name.'</span></a></div>';}</div>
