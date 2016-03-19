d7e04164ca1242ab91703eafa5b0504e21cf17d2
wkwkrnht <wkwkrnht@gmail.com>
2016-03-20 01:49:41 +0900

    Revert "Revert "fix""
    This reverts commit 2ad6c4da7e39844e789c9c3176b8b4823fad0041.

--- parts/related.php
+++ parts/related.php
@@ -1,13 +1,9 @@
 <div class="content">
 <?php $categories=get_the_category($post->ID);$category_ID=array();
 foreach($categories as $category):array_push($category_ID,$category->cat_ID);endforeach;
-$args=array('posts_per_page'=>6,'post__not_in'=>array($post->ID),'category__in'=>$category_ID,'orderby'=>'rand',);
+$args=array('posts_per_page'=>3,'post__not_in'=>array($post->ID),'category__in'=>$category_ID,'orderby'=>'rand',);
 $query=new WP_Query($args);
   if($query->have_posts()):?>
-<<<<<<< HEAD
-=======
-    <div class="content">
->>>>>>> parent of e87b776... fix
       <?php while($query->have_posts()):$query->the_post();?>
         <a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>"><div class="related">
           <?php if(has_post_thumbnail()):echo get_the_post_thumbnail($post->ID,'related',array('class'=>'thumb'));else:echo'<img src="http://wkwkrnht.wp.xdomain.jp/wp-content/themes/2015-for-wkwkrnht/img/no-img.png" class="thumb" alt="no_thumbnail" width="150px" height="150px"/>';endif;
@@ -16,12 +12,7 @@ $query=new WP_Query($args);
       <?php endwhile;?>
     <?php wp_reset_postdata();
   else:?>
-<<<<<<< HEAD
     <?php $args=array('numberposts'=>3,'orderby'=>'rand','post_status'=>'publish','offset'=>1);
-=======
-    <div class="content">
-    <?php $args=array('numberposts'=>6,'orderby'=>'rand','post_status'=>'publish','offset'=>1);
->>>>>>> parent of e87b776... fix
     $rand_posts=get_posts($args);
     foreach($rand_posts as $post):?>
       <a href="<?php the_permalink()?>" title="<?php the_title_attribute();?>"><div class="related">
