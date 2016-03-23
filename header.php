9259f7a5ab2072efb1dfc044dc72cbc4acd852e0
wkwkrnht <wkwkrnht@gmail.com>
2016-03-23 19:35:36 +0900

    fix

--- header.php
+++ header.php
@@ -54,7 +54,5 @@
 <body <?php body_class();?>>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e('Skip to content','twentyfifteen');?></a>
-	<section id="sidebar" class="sidebar">
-		<?php get_sidebar();?>
-	</section>
+	<?php get_sidebar();?>
 	<section id="content" class="site-content">
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,4 @@
-<section id="secondary" class="secondary">
+<section id="sidebar" class="sidebar">
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<button class="secondary-toggle"><?php _e('Menu and widgets','twentyfifteen');?></button>
@@ -9,21 +9,23 @@
 			<?php endif;?>
 		</div>
 	</header>
-	<?php if(has_nav_menu('primary')):?>
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<?php wp_nav_menu(array('menu_class'=>'nav-menu','theme_location'=>'primary',));?>
-		</nav>
-	<?php endif;?>
-	<?php if(has_nav_menu('social')):?>
-		<nav id="social-navigation" class="social-navigation" role="navigation">
-			<?php wp_nav_menu(array('theme_location'=>'social','depth'=>1,'link_before'=>'<span class="screen-reader-text">','link_after'=>'</span>',));?>
-		</nav>
-	<?php endif;?>
-	<?php if(is_active_sidebar('sidebar-1')):?>
-		<div id="widget-area" class="widget-area" role="complementary"><?php dynamic_sidebar('sidebar-1');?></div>
-	<?php endif;?>
-	<footer id="site-footer">
-		<div class="site-info"role="contentinfo"><?php do_action('twentyfifteen_credits');?></div>
-		<div class="copyright">&copy;2016<?php bloginfo('name');?></div>
-	</footer>
+	<section id="secondary" class="secondary">
+		<?php if(has_nav_menu('primary')):?>
+			<nav id="site-navigation" class="main-navigation" role="navigation">
+				<?php wp_nav_menu(array('menu_class'=>'nav-menu','theme_location'=>'primary',));?>
+			</nav>
+		<?php endif;?>
+		<?php if(has_nav_menu('social')):?>
+			<nav id="social-navigation" class="social-navigation" role="navigation">
+				<?php wp_nav_menu(array('theme_location'=>'social','depth'=>1,'link_before'=>'<span class="screen-reader-text">','link_after'=>'</span>',));?>
+			</nav>
+		<?php endif;?>
+		<?php if(is_active_sidebar('sidebar-1')):?>
+			<div id="widget-area" class="widget-area" role="complementary"><?php dynamic_sidebar('sidebar-1');?></div>
+		<?php endif;?>
+		<footer id="site-footer">
+			<div class="site-info"role="contentinfo"><?php do_action('twentyfifteen_credits');?></div>
+			<div class="copyright">&copy;2016<?php bloginfo('name');?></div>
+		</footer>
+	</section>
 </section>
