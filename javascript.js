a40bd488c6951e1689f2b56284871e70b7f27726
wkwkrnht <wkwkrnht@gmail.com>
2016-03-23 19:30:15 +0900

    fix

--- javascript.js
+++ javascript.js
@@ -2,4 +2,5 @@ function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)}(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 ga('create',<?php echo get_option('Google_Analytics');?>,'auto');ga('send','pageview');
-jQuery(function(){jQuery(document).ready(function(){jQuery('code').each(function(i,block){hljs.highlightBlock(block);});});jQuery('#page-top').click(function(){jQuery('.content').animate({scrollTop:0},500);return false;});});
+jQuery(function(){jQuery(document).ready(function(){jQuery('code').each(function(i,block){hljs.highlightBlock(block);});});});
+jQuery(function(){jQuery('#page-top').click(function(){jQuery('.content').animate({scrollTop:0},500);return false;});});
