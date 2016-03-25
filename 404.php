2486457e41c1010f88e609541c0d90fc6cd9f342
wkwkrnht <wkwkrnht@gmail.com>
2016-03-25 21:31:16 +0900

    fix

--- 404.php
+++ 404.php
@@ -4,11 +4,10 @@
 			<section class="error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php _e('Oops! That page can&rsquo;t be found.','twentyfifteen');?></h1>
-				</header>
-				<div class="page-content">
 					<p><?php _e('It looks like nothing was found at this location. Maybe try a search?','twentyfifteen');?></p>
 					<?php get_search_form();?>
-				</div>
+				</header>
+				<div class="page-content"><?php wp_tag_cloud();?></div>
 			</section>
 		</main>
 	</div>
--- author-bio.php
+++ author-bio.php
@@ -1,10 +1,9 @@
 <div class="author-info">
-	<h2 class="author-heading"><?php _e('Published by','twentyfifteen');?></h2>
+	<h2 class="author-heading"><?php echo get_the_author();?></h2>
 	<div class="author-avatar">
 		<?php $author_bio_avatar_size=apply_filters('twentyfifteen_author_bio_avatar_size',56);echo get_avatar(get_the_author_meta('user_email'),$author_bio_avatar_size);?>
 	</div>
 	<div class="author-description">
-		<h3 class="author-title"><?php echo get_the_author();?></h3>
 		<p class="author-bio">
 			<?php the_author_meta('description');?>
 			<a class="author-link" href="<?php echo esc_url(get_author_posts_url(get_the_author_meta('ID')));?>" rel="author">
