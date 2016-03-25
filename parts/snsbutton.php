a60b69558be65a2ee2c874928087cb7198f04159
wkwkrnht <wkwkrnht@gmail.com>
2016-03-25 21:59:03 +0900

    fix

--- parts/snsbutton.php
+++ parts/snsbutton.php
@@ -10,7 +10,7 @@
   <div class="content">
     <a href="https://plus.google.com/share?url=<?php echo get_permalink()?>" target="_blank"><div class="g1 button"></div></a>
     <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink()?>&amp;title=<?php echo trim(wp_title('',false));?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="linkedin button"></div></a>
-    <?php if(is_pushnotice_dsp())):?>
+    <?php if(is_pushnotice_dsp()):?>
       <a href="<?php echo get_option('Pushnotice_URL');?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="pushnotice button"></div></a>
     <?php else:?>
       <a href="https://push.dog/subscribe?url=<?php echo esc_url(home_url());?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="pushnotice button"></div></a>
