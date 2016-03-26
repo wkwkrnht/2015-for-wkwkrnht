ba010663f80f0655b00ad55c6dffe291cdce2c31
wkwkrnht <wkwkrnht@gmail.com>
2016-03-26 16:13:02 +0900

    fix

--- amp.php
+++ amp.php
@@ -67,7 +67,7 @@
 		<div class="meta"><?php twentyfifteen_entry_meta();?></div>
 	</header>
 	<section>
-		<?php while(have_posts()):the_content();endwhile;?>
+		<?php query_posts('posts_per_page=1');if(have_posts()):while(have_posts()):the_post();the_content();endwhile;endif;?>
 	</section>
 	<?php remove_filter('the_content',array($GLOBALS['wp_embed'],'autoembed'),8);
 	$content=apply_filters('the_content',get_the_content());$content=str_replace(']]>',']]&gt;',$content);
