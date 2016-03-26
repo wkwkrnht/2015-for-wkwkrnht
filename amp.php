28e4440b762deddea2d65792225c39aefef6bb2b
wkwkrnht <wkwkrnht@gmail.com>
2016-03-26 18:20:17 +0900

    fix

--- amp.php
+++ amp.php
@@ -66,12 +66,12 @@
 			$cat = is_single() ? get_the_category():array(get_category($cat));
 			if($cat&&!is_wp_error($cat)){$par=get_category($cat[0]->parent);
 			echo'<a href="'.home_url().'" itemprop="url"><span itemprop="title">Home</span></a><span class="sp">/</span>';
-			while($par&&!is_wp_error($par$par->term_id != 0){$echo='<a href="'.get_category_link($par->term_id).'" itemprop="url"><span itemprop="title">'.$par->name.'</span></a><span class="sp">/</span>'.$echo;$par=get_category($par->parent);}
+			while($par&&!is_wp_error($par)&&$par->term_id != 0){$echo='<a href="'.get_category_link($par->term_id).'" itemprop="url"><span itemprop="title">'.$par->name.'</span></a><span class="sp">/</span>'.$echo;$par=get_category($par->parent);}
 			echo $echo.'<a href="'.get_category_link($cat[0]->term_id).'" itemprop="url"><span itemprop="title">'.$cat[0]->name.'</span></a>';}endif;?></div>
 		<div class="meta"><?php twentyfifteen_entry_meta();?></div>
 	</header>
 	<section>
-		<?php query_posts('posts_per_page=1');if(have_posts()):while(have_posts()):the_post();the_content();endwhile;endif;?>
+		<?php if(empty($post->post_password))echo apply_filters('the_content',$post->post_content);endif;?>
 	</section>
 	<?php remove_filter('the_content',array($GLOBALS['wp_embed'],'autoembed'),8);
 	$content=apply_filters('the_content',get_the_content());$content=str_replace(']]>',']]&gt;',$content);
