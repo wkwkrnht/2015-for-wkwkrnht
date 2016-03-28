9712fba36907e5817422049991914aaf701d3c3c
wkwkrnht <wkwkrnht@gmail.com>
2016-03-28 18:32:12 +0900

    fix

--- javascript.js
+++ javascript.js
@@ -1,3 +1,2 @@
 jQuery(document).ready(function(){jQuery('#movetop').click(function(){jQuery('body,html').animate({scrollTop:0},500);});});
 jQuery(document).ready(function(){jQuery('code').each(function(i,block){hljs.highlightBlock(block);});});
-jQuery(document).ready(function(){jQuery('.embedly-card').embedly({key:'<?php echo get_option('embedly_API_Key');?>'});});
