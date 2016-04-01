373886867a6045f1bf9defad5054153b1d5d5a5c
wkwkrnht <wkwkrnht@gmail.com>
2016-04-01 22:10:49 +0900

    fix

--- parts/adminnav.php
+++ parts/adminnav.php
@@ -3,7 +3,7 @@
 		<a href="<?php echo esc_url(home_url());?>/wp-login.php" target="_blank" class="loginbutton"></a>
 	<?php else:?>
 		<a href="<?php echo esc_url(home_url());?>/wp-admin/post-new.php" target="_blank" class="postnewbutton"></a>
-		<?php if(is_home()):edit_post_link();echo'<a href="wlw://wkwkrnht.gegahost.net/?postid=';echo the_ID();echo'" class="wlwedit"></a>';else:echo'<a href="wlw://wkwkrnht.gegahost.net/?postid=';echo the_ID();echo'" class="wlwedit"></a>';endif;?>
+		<?php if(is_home()):edit_post_link( ,);echo'<a href="wlw://wkwkrnht.gegahost.net/?postid=';echo the_ID();echo'" class="wlwedit"></a>';else:echo'<a href="wlw://wkwkrnht.gegahost.net/?postid=';echo the_ID();echo'" class="wlwedit"></a>';endif;?>
 		<a href="<?php echo esc_url(home_url());?>/wp-admin/" target="_blank" class="adminmenubutton"></a>
 	<?php endif;?>
 </nav>
