d376caa60b3c0073a09bda572a05b6aa4668614b
wkwkrnht <wkwkrnht@gmail.com>
2016-04-15 23:34:08 +0900

    fix

--- parts/adminnav.php
+++ parts/adminnav.php
@@ -8,8 +8,8 @@
 		<a href="<?php echo esc_url(home_url());?>/wp-admin/" target="_blank" class="adminmenubutton"></a>
 	<?php endif;?>
 	<?php if(is_pushnotice_dsp()):?>
-      <a href="<?php echo get_option('Pushnotice_URL');?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="pushnotice"></div></a>
+      <a href="<?php echo get_option('Pushnotice_URL');?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><span class="pushnotice"></span></a>
     <?php else:?>
-      <a href="https://push.dog/subscribe?url=<?php echo esc_url(home_url());?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="pushnotice"></div></a>
+      <a href="https://push.dog/subscribe?url=<?php echo esc_url(home_url());?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><span class="pushnotice"></span></a>
     <?php endif;?>
 </nav>
--- parts/sharebutton.php
+++ parts/sharebutton.php
@@ -1,4 +1,4 @@
-<script src="//static.evernote.com/noteit.js"></script>
+<!--<script src="//static.evernote.com/noteit.js"></script>-->
 <div id="fb-root"></div>
 <script>(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(d.getElementById(id))return;js=d.createElement(s);js.id=id;js.src="//connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v2.6&appId=951165034967338";fjs.parentNode.insertBefore(js,fjs);}(document,'script','facebook-jssdk'));</script>
 <div class="snsshare">
