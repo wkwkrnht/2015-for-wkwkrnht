e8c6a54b797fba1e502c4cda00cf83b45ed5e7bd
wkwkrnht <wkwkrnht@gmail.com>
2016-04-15 23:28:30 +0900

    fix

--- header.php
+++ header.php
@@ -64,13 +64,14 @@
 	<section id="sidebar" class="sidebar">
 		<header id="masthead" class="site-header" role="banner">
 			<div class="site-branding">
-				<?php the_site_logo();?>
+				<?php twentyfifteen_the_custom_logo();?>
 				<button class="secondary-toggle"><?php _e('Menu and widgets','twentyfifteen');?></button>
 				<?php if(is_front_page()&&is_home()):?>
 					<h1 class="site-title"><a href="<?php echo esc_url(home_url('/'));?>" rel="home"><?php bloginfo('name');?></a></h1>
 				<?php else:?>
 					<p class="site-title"><a href="<?php echo esc_url(home_url('/'));?>" rel="home"><?php bloginfo('name');?></a></p>
 				<?php endif;?>
+				<?php $description=get_bloginfo('description','display');if($description||is_customize_preview()):echo'<p class="site-description">' . $description . '</p>';endif;?>
 			</div>
 		</header>
 		<?php get_sidebar();?>
