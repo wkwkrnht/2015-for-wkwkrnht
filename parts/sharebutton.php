8627e716a81db5971c2a71380d367d66c747d2e9
wkwkrnht <wkwkrnht@gmail.com>
2016-04-16 18:44:57 +0900

    fix

--- parts/sharebutton.php
+++ parts/sharebutton.php
@@ -11,7 +11,7 @@
   </div>
   <div class="content">
     <div class="fb-save fb button" data-uri="<?php echo get_permalink();?>"></div>
-	<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink();?>&media=<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID));?>&description=<?php echo str_replace(array("<p>","</p>","<a>","</a>","<span>","</span>",),"",get_the_excerpt());?>"><div class="printerest button"></div></a>
+	<a href="http://pinterest.com/pin/create/button/?url=<?php the_permalink();?>&media=<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID));?>"><div class="printerest button"></div></a>
 	<a href="http://getpocket.com/edit?url=<?php the_permalink();?>&amp;title=<?php echo trim(wp_title('',false));?>" onclick="window.open(this.href,'window','width=550,height=350,menubar=no,toolbar=no,scrollbars=yes');return false;"><div class="pocket button"></div></a>
     <a href="https://www.tumblr.com/widgets/share/tool?canonicalUrl=<?php echo get_permalink();?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="tumblr button"></div></a>
 	<?php if(function_exists('print_embed_sharing_button')){print_embed_sharing_button();};?>
