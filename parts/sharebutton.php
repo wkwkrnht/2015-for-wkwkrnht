613a41a26d1948d8d93af33a2106949f80fc0cea
wkwkrnht <wkwkrnht@gmail.com>
2016-04-17 13:42:03 +0900

    fix

--- parts/sharebutton.php
+++ parts/sharebutton.php
@@ -2,7 +2,7 @@
   <div class="content">
     <a href="https://twitter.com/share?url=<?php echo get_permalink();?>&amp;text=<?php echo trim(wp_title('',false));?>&amp;via=<?php the_author_meta('twitter');?>" onclick="window.open(encodeURI(decodeURI(this.href),'tweetwindow','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;" target="_blank"><div class="tweet button"></div></a>
     <a href="http://www.facebook.com/share.php?u=<?php echo rawurlencode(get_permalink());?>" target="_blank"><div class="fb-like fb button"></div></a>
-    <a href="http://line.me/R/msg/text/?<?php the_title(); ?>%0D%0A<?php the_permalink();?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/img/line.svg" alt="LINEで送る" class="line button"/></a>
+    <a href="http://line.me/R/msg/text/?<?php the_title();?>%0D%0A<?php the_permalink();?>" target="_blank"><img src="<?php echo get_stylesheet_directory_uri();?>/img/line.svg" alt="LINEで送る" class="line button"/></a>
     <a href="https://plus.google.com/share?url=<?php echo get_permalink();?>" target="_blank"><div class="g1 button"></div></a>
     <a href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo get_permalink();?>&amp;title=<?php echo trim(wp_title('',false));?>" onclick="window.open(this.href,'window','width=550,height=450,personalbar=0,toolbar=0,scrollbars=1,resizable=1');return false;"><div class="linkedin button"></div></a>
   </div>
