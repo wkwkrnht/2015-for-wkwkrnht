881de6cc9b9349c7ebe85eaacd263c27163b7713
wkwkrnht <wkwkrnht@gmail.com>
2016-04-30 11:33:18 +0900

    fix

--- sidebar-8.php
+++ sidebar-8.php
@@ -1,10 +1,8 @@
 <footer class="entry-footer">
 	<h2><?php echo get_option('entryfooter_txt')?></h2>
-	<div id="flex">
-		<?php if(is_active_sidebar('8')):
-			dynamic_sidebar('8');
-		else:
-			get_template_part('parts/snsbutton');get_template_part('parts/related');
-		endif;?>
-	</div>
+	<?php if(is_active_sidebar('8')):
+		dynamic_sidebar('8');
+	else:
+		get_template_part('parts/snsbutton');get_template_part('parts/related');
+	endif;?>
 </footer>
